abe177b4b42b661199686d00099d1da5901ed3bc
Aaditya Parashar <aaditya.p0901@gmail.com>
2025-08-11 18:11:45 +0530

    Changed post area visual properties

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
     include("html_header.php");
     include("home_nav.php");
 ?>
-        <div class = "container" style = "text-align: center" id = "posts_container"><div class = "row w-100" id = "posts_area"></div></div>
+        <div class = "container mt-5" style = "text-align: center" id = "posts_container"><div class = "row w-100" id = "posts_area"></div></div>
 <?php
     $result = $connection->query("select Description, Username, URL from posts order by ID desc limit 25;");
     if ($result->num_rows == 0) { ?>
@@ -22,8 +22,8 @@
         $description = $row["Description"];
         $url = $row["URL"];
         $username = $row["Username"];
-        $post = "<div class = 'card col-md-6'>".
-            "<img class = 'card-img-top' src = '$url' alt = '$url'>".
+        $post = "<div class = 'card' style = 'width: 45%; margin: 0px 2.5%'>".
+            "<div class = 'h-100 d-flex justify-content-center align-items-center'><img class = 'card-img-top' src = '$url' alt = '$url'></div>".
             "<div class = 'card-body'>$description</div>".
             "<div class = 'card-footer'>$username</div></div>"; ?>
         <script>
