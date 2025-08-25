feb177396cfa0c0e465564fb3191f0b9c21bb83c
Aaditya Parashar <aaditya.p0901@gmail.com>
2025-08-25 22:43:05 +0530

    Changed to Bootstrap CDN

--- html_header.php
+++ html_header.php
@@ -2,7 +2,8 @@
     <head>
         <title>Simple Social Network - 2</title>
         <link rel = "stylesheet" href = "style.css">
-        <link rel = "stylesheet" href = "bootstrap/bootstrap.min.css"><script src = "bootstrap/bootstrap.bundle.min.js"></script>
+        <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel = "stylesheet" integrity = "sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin = "anonymous">
+        <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin = "anonymous"></script>
         <link rel = "stylesheet" href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle,add_circle,delete_forever,edit,home,menu,passkey">
         <meta name = "viewport" content = "width = device-width, initial-scale = 1">
         <style>
